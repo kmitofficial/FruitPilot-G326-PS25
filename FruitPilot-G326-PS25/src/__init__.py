c1d8a3bf8b5fa130ead8781dfde141befb2c1190
Sairam <sairammekala7@gmail.com>
2025-06-02 00:31:38 +0530

    Deleted useless files

--- FruitPilot-G326-PS25/src/__init__.py
+++
@@ -1,2 +0,0 @@
-# File: /FruitPilot-G326-PS25/FruitPilot-G326-PS25/src/__init__.py
-# This file is intentionally left blank.
--- FruitPilot-G326-PS25/src/detection.py
+++
@@ -1,4 +0,0 @@
-def get_detections(frame):
-    model = YOLO('../old_150.pt')  # Load the YOLO model
-    results = model(frame)  # Perform detection
-    return results  # Return detection results
